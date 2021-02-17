272847804b34404b31831a240cb01f4550193af3
Loïc Frémont <lc.fremont@gmail.com>
2021-02-17 11:28:24 +0100

    change driver name

--- src/Component/Definition/Grid.php
+++ src/Component/Definition/Grid.php
@@ -63,6 +63,11 @@ class Grid
         return $this->driver;
     }
 
+    public function setDriver(string $driver): void
+    {
+        $this->driver = $driver;
+    }
+
     public function getDriverConfiguration(): array
     {
         return $this->driverConfiguration;
